5abb737b913b224592aab04e9b5d7a33e7397058
thsrnrwls <bluenet87@naver.com>
2015-09-07 14:08:26 +0900

    fixed#1 스켈레톤 작성.

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int add(int, int)
+int sub(int, int)
+int div(int, int)
+int multi(int, int)
+
+int main(int argc, char* argv[])
+{
+	int a, b, c;
+	char operator;
+
+	printf("please enter a experssion");
+
+	return 0;
+}
